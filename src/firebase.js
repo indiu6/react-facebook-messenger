cc58155d143f070dec6ec9ac4727491b01ebacce
indiu6 <hewas6@gmail.com>
2021-01-15 23:07:52 -0500

    edit auth

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyAl_fAOvdJnTeHOSM--q0rsIbrt-5X4p3Q',
-  authDomain: 'clone-fcd1f.firebaseapp.com',
-  projectId: 'clone-fcd1f',
-  storageBucket: 'clone-fcd1f.appspot.com',
-  messagingSenderId: '599958070569',
-  appId: '1:599958070569:web:612f73ae33779923e18338',
-  measurementId: 'G-8WQCY4JLFF',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUR_ID,
 });
 
 const db = firebaseApp.firestore();
